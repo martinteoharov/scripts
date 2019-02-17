1bf659703f21e5547bbc37e49bd2312ea72deb85
BigBadWolfMeow <martin.teoharov15@gmail.com>
2019-02-17 10:33:50 +0200

    little tweaks

--- bulk_rename_and_resize.sh
+++ bulk_rename_and_resize.sh
@@ -1,6 +1,7 @@
 let cnt=1
 let smth=9
 let smth0=99
+let smth1=999
 let zero=0
 check=600
 image=img
@@ -8,16 +9,7 @@ for i in *
 do 
 	if [[ $i == *"img"* ]]
 	then
-		echo " file has been processed already"
 		let cnt+=1
-
-		status=$(file img1248.png | awk '{ print $5 }')
-		if [ $status -ne $check ]
-		then
-			convert $i -resize 600x400 $i
-			echo $status
-		fi
-
 	else
 		if [[ $i != *".png"* ]]
 		then
@@ -27,14 +19,19 @@ do
 			echo -n $cnt
 			if [[ "$cnt" -le "$smth" ]]
 			then
+				mv $i "$image$zero$zero$zero$cnt.png"; let cnt+=1;
+				echo $image$zero$zero$zero$cnt.png
+				convert $image$zero$zero$zero$cnt.png -resize 600x400 $image$zero$zero$zero$cnt.png
+			elif [[ "$cnt" -le "smth0" ]]
+			then
 				mv $i "$image$zero$zero$cnt.png"; let cnt+=1;
 				echo $image$zero$zero$cnt.png
 				convert $image$zero$zero$cnt.png -resize 600x400 $image$zero$zero$cnt.png
-			elif [[ "$cnt" -le "smth0" ]]
+			elif [[ "$cnt" -le "smth1" ]]
 			then
 				mv $i "$image$zero$cnt.png"; let cnt+=1;
 				echo $image$zero$cnt.png
-				convert $image$zero$cnt.png -resize 600x400 $image$zero$zero$cnt.png
+				convert $image$zero$cnt.png -resize 600x400 $image$zero$cnt.png
 			else
 				mv $i "$image$cnt.png"; let cnt+=1;
 				echo $image$cnt.png
@@ -43,12 +40,15 @@ do
 
 		fi
 
-		status=$(file img1248.png | awk '{ print $5 }')
-		if [ $status -ne $check ]
-		then
-			convert $i -resize 600x400 $i
-			echo $status
-		fi
-
+	fi
+done
+for m in *
+do
+	status=$(file $m | awk '{ print $5 }')
+	if [[ $status -ne $check ]]
+	then
+		convert $m -resize 600x400 $m
+		echo -n $cnt " "
+		echo $status
 	fi
 done
